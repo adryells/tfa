7ac766e14fc583f452186799d39f9c5abf55d62d
adryellsz <adryellpaulo@gmail.com>
2023-09-09 22:55:49 -0300

    Update vercel.json + back with uvicorn.run

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.services.router import graphql_router, graphql_app
@@ -20,12 +20,6 @@ app.add_route("/graphql", graphql_app)
 
 @app.get("/")
 def root():
-    ui = ''
-    return {"Hello": "World"}
-
-
-@app.get("/a")
-def hya(request: Request, session: Session = Depends(main_session)):
     return {"Hello": "World"}
 
 
